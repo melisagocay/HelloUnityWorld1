160ed9e58615fd22f5d1fb7c0be7ec05b099b5f3
melisa gocay <mgocay@knights.ucf.edu>
2019-07-01 17:30:02 -0400

    Added the words Hello world from Melisa after user presses space

---
+++ HelloUnityWorld1/Assets/chan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class chan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextMeshProUGUI myText = GetComponent<TextMeshProUGUI>();
+        if (Input.GetKeyDown("space")) {
+            myText.SetText("Hello World From Melisa!");
+            Debug.Log ("Key Pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+}
